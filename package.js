f21aacb3c7c68ff78a12b44f71e05cffd547947b
Robert Dickert <robert.dickert@gmail.com>
2016-08-19 08:56:24 -0600

    lower required Meteor version to 1.3

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.4.0.1');
+  api.versionsFrom('1.3');
   api.use('ecmascript');
   api.use('session');
   api.use('blaze-html-templates');
