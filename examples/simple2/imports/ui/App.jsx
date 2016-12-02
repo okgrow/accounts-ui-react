e6c5056165116090f585c39a31ee2679b7f5773e
Aryan Goharzad <arrygoo@gmail.com>
2016-12-02 11:33:59 -0500

    Added another example, going to replace simple with simple2 before merging

---
+++ examples/simple2/imports/ui/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+// App component - represents the whole app
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+
+        </ul>
+      </div>
+    );
+  }
+}
