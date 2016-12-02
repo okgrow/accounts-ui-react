c136825227764a6d8e076818f250316ceb1fb79e
Aryan Goharzad <arrygoo@gmail.com>
2016-12-02 11:11:08 -0500

    Added a simple example to the project

---
+++ examples/simple/imports/ui/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { LoginButtons } from 'okgrow:accounts-ui-react';
+
+// App component - represents the whole app
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Example App</h1>
+
+      </div>
+    );
+  }
+}
